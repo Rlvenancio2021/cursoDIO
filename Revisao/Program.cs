2d1ddf580a9a050b5609698133be1f2d56da2641
Rodrigo <rlvenancio@gmail.com>
2021-11-09 20:11:44 -0300

    adicionar método

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -7,21 +7,13 @@ namespace Revisao
         static void Main(string[] args)
         {
             //## Imprimir menu para o usuário ##
-            Console.WriteLine("Informe a opção desejada");
-            Console.WriteLine("1- Inserir novo aluno");
-            Console.WriteLine("2- Listar alunos");
-            Console.WriteLine("3- Calcular média geral");
-            Console.WriteLine("X- Sair");
-            Console.WriteLine();
-
-            //## Solicita uma opção ao usuário ##
-            string opcaoUsuario = Console.ReadLine();
+            string opcaoUsuario = ObterOpcaoUsusario();
             //Opção para dar obter uma informação do usuário
 
             while (opcaoUsuario.ToUpper() != "X")
             // != significa Diferente de
             {
-                switch(opcaoUsuario)
+                switch (opcaoUsuario)
                 {
                     case "1":
                         //TODO: adicionar aluno
@@ -36,7 +28,15 @@ namespace Revisao
                         throw new ArgumentOutOfRangeException();
                         //Caso digite uma opção invalida ele irá indicar que o valo informado esta fora do range de opções
                 }
-            //## Após execução solicitar uma nova opção ao usuário ##
+               
+                opcaoUsuario = ObterOpcaoUsusario();
+
+            }
+        }
+
+        //# Criar um método #
+        private static string ObterOpcaoUsusario()
+        {
             Console.WriteLine("Informe a opção desejada");
             Console.WriteLine("1- Inserir novo aluno");
             Console.WriteLine("2- Listar alunos");
@@ -44,9 +44,9 @@ namespace Revisao
             Console.WriteLine("X- Sair");
             Console.WriteLine();
 
-            opcaoUsuario = Console.ReadLine();
-
-            }            
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
